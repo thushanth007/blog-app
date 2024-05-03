b1bde372d5c96de1a1c8ee33828a0ef7415254a5
thushanth007 <2358789@swansea.ac.uk>
2024-05-03 05:04:55 +0100

    Alignment adjustment

--- config/services.php
+++ config/services.php
@@ -31,6 +31,16 @@ return [
         ],
     ],
 
-    'github' => [ 'client_id' => env('GITHUB_CLIENT_ID'), 'client_secret' => env('GITHUB_CLIENT_SECRET'), 'redirect' => '/auth/github/callback', ], 'google' => [ 'client_id' => env('GOOGLE_CLIENT_ID'), 'client_secret' => env('GOOGLE_CLIENT_SECRET'), 'redirect' => '/auth/google/callback', ],
+    'github' => [ 
+    'client_id' => env('GITHUB_CLIENT_ID'),
+    'client_secret' => env('GITHUB_CLIENT_SECRET'), 
+    'redirect' => '/auth/github/callback', 
+], 
+
+    'google' => [ 
+    'client_id' => env('GOOGLE_CLIENT_ID'), 
+    'client_secret' => env('GOOGLE_CLIENT_SECRET'), 
+    'redirect' => '/auth/google/callback', 
+],
 
 ];
